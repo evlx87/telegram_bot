617b20808ca6449584d2244a0dd334f32af31c77
evlx87 <evlx@mail.ru>
2023-05-26 21:33:11 +0300

    add config.json and error_messages.json

--- bot.py
+++ bot.py
@@ -1,35 +1,43 @@
-"""Простой телеграм бот"""
 import json
 import telebot
 from telebot import apihelper
 
-# Получаем данные настроек из JSON файла config.json
-with open('config.json', 'r', encoding='utf-8') as f:
-    bot_api = json.load(f)
+# Загрузка конфигурации из файла JSON
+try:
+    with open('config.json', 'r', encoding='utf-8') as f:
+        bot_api = json.load(f)
+except FileNotFoundError:
+    print('Файл конфигурации не найден')
+    exit()
+except json.JSONDecodeError:
+    print('Неверный файл конфигурации')
+    exit()
 
-# Подключаем бота с полученными настройками
+# Подключение бота с использованием конфигурации
 bot = telebot.TeleBot(bot_api['token'])
 apihelper.proxy = {'https': bot_api['proxy_url']}
 
-"""На случай непонятного запроса"""
-error_message = "В следующих версиях я смогу научиться отвечать на этот запрос"
+# Сообщение об ошибке
+with open('error_messages.json', 'r', encoding='utf-8') as f:
+    error_messages = json.load(f)
 
 
+# Отправка приветственного сообщения
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    """Сообщение приветствия"""
     bot.send_message(message.chat.id, 'Привет, землянин!'
-                                      '\nМеня зовут TestBot и буду рад помочь тебе.'
-                                      '\nНапиши /help чтобы узнать что я умею')
+                     '\nМеня зовут TestBot и буду рад помочь тебе.'
+                     '\nНапиши /help чтобы узнать что я умею')
 
 
+# Обработка пользовательского ввода
 @bot.message_handler(content_types=['text'])
 def help_message(message):
-    """Функция диалога с пользователем по различным сценариям в зависимости"""
     if message.text == "/help":
-        bot.send_message(message.from_user.id, error_message)
+        bot.send_message(message.from_user.id, error_messages['unknown_command'])
     else:
-        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+        bot.send_message(message.from_user.id, error_messages['unknown_input'])
 
 
+# Запуск бота
 bot.polling()
